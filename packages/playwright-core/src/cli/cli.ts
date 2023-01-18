2f6366994999997222cee10bfe7dd150d378bf65
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2023-01-17 21:43:40 -0800

    remove broken kiosk

--- packages/playwright-core/src/cli/cli.ts
+++ packages/playwright-core/src/cli/cli.ts
@@ -425,7 +425,7 @@ async function launchContext(options: Options, headless: boolean, executablePath
       '--disable-dev-shm-usage',
       `--app=${process.env.START_URL}`,
       `--user-agent=${process.env.USER_AGENT}`,
-      `--kiosk=${process.env.KIOSK}`,
+      // `--kiosk=${process.env.KIOSK}`,
       '--remote-debugging-port=9333'
     ],
     env: {
